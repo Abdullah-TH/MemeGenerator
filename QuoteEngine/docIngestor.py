de89b84da9ecdc10ce241502d5610b33b8a14475
Abdullah <abdullah.th@me.com>
2020-03-09 19:17:41 +0300

    Add skeleton code for classes that realize the IngestorInterface
    CSVIngestor
    DOCIngestor
    PDFIngestor
    TXTIngestor

---
+++ QuoteEngine/docIngestor.py
@@ -0,0 +1,13 @@
+from .ingestorInterface import IngestorInterface
+from .quoteModel import QuoteModel
+
+
+class DOCIngestor(IngestorInterface):
+
+    @classmethod
+    def can_ingest(cls, path: str) -> bool:
+        pass
+
+    @classmethod
+    def parse(cls, path: str) -> list[QuoteModel]:
+        pass
