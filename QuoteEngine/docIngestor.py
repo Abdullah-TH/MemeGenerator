ee77f03fe18f0c7da4e5a6b4db03ec4e658b8e59
Abdullah <abdullah.th@me.com>
2020-03-10 17:56:20 +0300

    Complete the implementation of DOCIngestor

--- QuoteEngine/docIngestor.py
+++ QuoteEngine/docIngestor.py
@@ -1,13 +1,28 @@
-from .ingestorInterface import IngestorInterface
-from .quoteModel import QuoteModel
+import re
+from typing import List
+from ingestorInterface import IngestorInterface
+from quoteModel import QuoteModel
+from docx import Document
 
 
 class DOCIngestor(IngestorInterface):
 
     @classmethod
     def can_ingest(cls, path: str) -> bool:
-        pass
+        file_extension = path[-4:].lower()
+        return file_extension == "docx"
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
-        pass
+        result = []
+        doc = Document(path)
+        for paragraph in doc.paragraphs:
+            if paragraph.text == "":
+                continue
+            body = cls.__parse_line(paragraph.text, '"', '"')
+            author = cls.__parse_line(paragraph.text, "- ", "")
+            result.append(QuoteModel(author, body))
+
+    @classmethod
+    def __parse_line(cls, string, start, end):
+        return string[string.find(start) + len(start):string.rfind(end)]
