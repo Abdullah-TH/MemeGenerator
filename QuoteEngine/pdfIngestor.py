1abcf6ac44633589894618be5b6b394388e8f3f9
Abdullah <abdullah.th@me.com>
2020-03-10 19:43:21 +0300

    Complete the implementation of PDFIngestor

--- QuoteEngine/pdfIngestor.py
+++ QuoteEngine/pdfIngestor.py
@@ -1,13 +1,29 @@
-from .ingestorInterface import IngestorInterface
-from .quoteModel import QuoteModel
+from typing import List
+from ingestorInterface import IngestorInterface
+from quoteModel import QuoteModel
+from pdfminer import high_level as pdf
 
 
 class PDFIngestor(IngestorInterface):
 
     @classmethod
     def can_ingest(cls, path: str) -> bool:
-        pass
+        file_extension = path[-3:].lower()
+        return file_extension == "pdf"
 
     @classmethod
-    def parse(cls, path: str) -> list[QuoteModel]:
-        pass
+    def parse(cls, path: str) -> List[QuoteModel]:
+        result = []
+        with open(path, 'rb') as file:
+            text = pdf.extract_text(path).strip().split("\n")
+            for line in text:
+                body = cls.__parse_line(line, '"', '"')
+                author = cls.__parse_line(line, "- ", "")
+                quote = QuoteModel(author, body)
+                result.append(quote)
+
+        return result
+
+    @classmethod
+    def __parse_line(cls, string, start, end):
+        return string[string.find(start) + len(start):string.rfind(end)]
