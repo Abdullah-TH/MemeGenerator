5f4bb30d01f50267dca8403c96a3c3f77f9e631a
Abdullah <abdullah.th@me.com>
2020-03-11 13:04:08 +0300

    Complete the implementation of TXTIngestor

--- QuoteEngine/txtIngestor.py
+++ QuoteEngine/txtIngestor.py
@@ -1,13 +1,27 @@
-from .ingestorInterface import IngestorInterface
-from .quoteModel import QuoteModel
+from typing import List
+from ingestorInterface import IngestorInterface
+from quoteModel import QuoteModel
 
 
 class TXTIngestor(IngestorInterface):
 
     @classmethod
     def can_ingest(cls, path: str) -> bool:
-        pass
+        file_extension = path[-3:].lower()
+        return file_extension == "txt"
 
     @classmethod
-    def parse(cls, path: str) -> list[QuoteModel]:
-        pass
+    def parse(cls, path: str) -> List[QuoteModel]:
+        result = []
+        with open(path, encoding='utf-8-sig') as file:
+            lines = file.read().splitlines()
+            for line in lines:
+                body = cls.__parse_line(line, '', ' -')
+                author = cls.__parse_line(line, "- ", "")
+                quote = QuoteModel(author, body)
+                result.append(quote)
+        return result
+
+    @classmethod
+    def __parse_line(cls, string, start, end):
+        return string[string.find(start) + len(start):string.rfind(end)]
