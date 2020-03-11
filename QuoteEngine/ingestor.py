bfed0dcbeba3d4beaf48c1a87b5571e9892d8159
Abdullah <abdullah.th@me.com>
2020-03-11 14:22:45 +0300

    Implement `Ingestor` that select the appropriate ingestor class based on file type

---
+++ QuoteEngine/ingestor.py
@@ -0,0 +1,30 @@
+from typing import List
+from ingestorInterface import IngestorInterface
+from quoteModel import QuoteModel
+from csvIngestor import CSVIngestor
+from docIngestor import DOCIngestor
+from pdfIngestor import PDFIngestor
+from txtIngestor import TXTIngestor
+
+
+class Ingestor(IngestorInterface):
+
+    @classmethod
+    def can_ingest(cls, path: str) -> bool:
+        return CSVIngestor.can_ingest(path) or \
+               DOCIngestor.can_ingest(path) or \
+               PDFIngestor.can_ingest(path) or \
+               TXTIngestor.can_ingest(path)
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        if path[-3:].lower() == 'csv':
+            return CSVIngestor.parse(path)
+        elif path[-4:].lower() == 'docx':
+            return DOCIngestor.parse(path)
+        elif path[-3:].lower() == 'pdf':
+            return PDFIngestor.parse(path)
+        elif path[-3:].lower() == 'txt':
+            return TXTIngestor.parse(path)
+        else:
+            raise Exception('File type is not supported')
