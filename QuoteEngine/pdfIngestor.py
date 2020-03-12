ff6a80f9f31988e07b9793d2fff57369f3b3b1ea
Abdullah <abdullah.th@me.com>
2020-03-12 13:01:05 +0300

    Update PDFIngestor to use pdftotext from CLI using subprocess module

--- QuoteEngine/pdfIngestor.py
+++ QuoteEngine/pdfIngestor.py
@@ -1,16 +1,29 @@
+import subprocess
+
 from typing import List
 from ingestorInterface import IngestorInterface
 from quoteModel import QuoteModel
-from pdfminer import high_level as pdf
-from quoteBuilder import QuoteBuilder
+from textIngestor import TextIngestor
 
 
 class PDFIngestor(IngestorInterface):
 
     file_extension = 'pdf'
+    txt_file_path = '_data/DogQuotes/DogQuotesPDF.txt'
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
-        with open(path, 'rb') as file:
-            lines = pdf.extract_text(path).strip().split("\n")
-            return QuoteBuilder.parse_quote(lines, "- ", "", '"', '"')
+        command = r'{} "{}" "{}" -enc UTF-8'.format('pdftotext', path, cls.txt_file_path)
+        subprocess.call(command, shell=True, stderr=subprocess.STDOUT)
+        cls.__clean_txt_file()
+        return TextIngestor.parse(cls.txt_file_path)
+
+    @classmethod
+    def __clean_txt_file(cls):
+        new_lines = []
+        with open(cls.txt_file_path) as file:
+            lines = file.readlines()
+            new_lines = [line.replace('"', '') for line in lines if not line.isspace()]
+
+        with open(cls.txt_file_path, 'w') as file:
+            file.writelines(new_lines)
