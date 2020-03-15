16c06d65be83e579e4b0cae292844a1648fdfd64
Abdullah Althobetey <abdullah.th@me.com>
2020-03-16 01:36:17 +0300

    Handle exceptions on MemeEngine

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -13,16 +13,34 @@ class MemeEngine:
         if width > 500:
             raise Exception('width should be at most 500px')
 
-        img = Image.open(img_path)
+        try:
+            img = Image.open(img_path)
+        except FileNotFoundError:
+            print(f'File not found on path: {img_path}')
+            return
+
         ratio = width / float(img.size[0])
         height = int(ratio * float(img.size[1]))
         img = img.resize((width, height), Image.NEAREST)
 
         img_draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype('./fonts/01211_AHDSANSB.ttf', size=20)
+        try:
+            font = ImageFont.truetype('./fonts/01211_AHDSANSB.ttf', size=20)
+        except OSError:
+            print("The file /fonts/01211_AHDSANSB.ttf is missing")
+            return
+
         random_x = randrange(width / 2)
         random_y = randrange(height - 30)
         img_draw.text((random_x, random_y), f'{text} - {author}', font=font, fill='white')
 
-        img.save(self.output_path)
+        try:
+            img.save(self.output_path)
+        except ValueError as error:
+            print(error)
+            return
+
         return self.output_path
+
+
+MemeEngine('./meme.jpg').make_meme('_data/photos/dog/xander_1.jpg', "Body texxxxt", "Author")
