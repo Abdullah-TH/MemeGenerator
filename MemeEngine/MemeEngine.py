2afab19de8f66ea01b7e78a77c33b90dd72cb0d1
Abdullah Althobetey <abdullah.th@me.com>
2020-03-16 01:55:44 +0300

    Forward exceptions to caller instead of handling them in make_meme method

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -20,36 +20,27 @@ class MemeEngine:
         :param author:
 
         :return: list of QuoteModel instances
+
+        :raise ValueError: if width > 500
+        :raise FileNotFoundError: if img_path is not found
+        :raise OSError: if font file at "./fonts/01211_AHDSANSB.ttf" is missing
+        :raise ValueError: if MemeEngine.output_path is invalid
         """
 
         if width > 500:
-            raise Exception('width should be at most 500px')
+            raise ValueError('width should be at most 500px')
 
-        try:
-            img = Image.open(img_path)
-        except FileNotFoundError:
-            print(f'File not found on path: {img_path}')
-            return
+        img = Image.open(img_path)
 
         ratio = width / float(img.size[0])
         height = int(ratio * float(img.size[1]))
         img = img.resize((width, height), Image.NEAREST)
 
         img_draw = ImageDraw.Draw(img)
-        try:
-            font = ImageFont.truetype('./fonts/01211_AHDSANSB.ttf', size=20)
-        except OSError:
-            print("The file /fonts/01211_AHDSANSB.ttf is missing")
-            return
-
+        font = ImageFont.truetype('./fonts/01211_AHDSANSB.ttf', size=20)
         random_x = randrange(width / 2)
         random_y = randrange(height - 30)
         img_draw.text((random_x, random_y), f'{text} - {author}', font=font, fill='white')
 
-        try:
-            img.save(self.output_path)
-        except ValueError as error:
-            print(error)
-            return
-
+        img.save(self.output_path)
         return self.output_path
