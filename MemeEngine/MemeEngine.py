b89782cdec6d7f559b2b2a45b59d6ec3faddbc39
Abdullah Althobetey <abdullah.th@me.com>
2020-03-16 01:17:55 +0300

    Rename instance variable of MemeEngine from path to output_path and use it as the path of the generated meme

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -6,13 +6,13 @@ from PIL import ImageFont
 class MemeEngine:
 
     def __init__(self, path):
-        self.path = path
+        self.output_path = path
 
     def make_meme(self, img_path, text, author, width=500):
         if width > 500:
             raise Exception('width should be at most 500px')
 
-        img = Image.open(self.path)
+        img = Image.open(img_path)
         ratio = width / float(img.size[0])
         height = int(ratio * float(img.size[1]))
         img = img.resize((width, height), Image.NEAREST)
@@ -21,6 +21,5 @@ class MemeEngine:
         font = ImageFont.truetype('./fonts/01211_AHDSANSB.ttf', size=20)
         img_draw.text((10, 30), f'{text} - {author}', font=font, fill='white')
 
-        output_path = './meme.jpg'
-        img.save(output_path)
-        return output_path
+        img.save(self.output_path)
+        return self.output_path
