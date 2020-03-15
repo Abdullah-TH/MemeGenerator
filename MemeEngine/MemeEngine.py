4e13ee3c15d2d6f26769f8cdaec4b3ee40aa6e27
Abdullah Althobetey <abdullah.th@me.com>
2020-03-16 01:02:58 +0300

    Implement resizing, drawing, and saving the meme image

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 
 class MemeEngine:
@@ -6,12 +8,19 @@ class MemeEngine:
     def __init__(self, path):
         self.path = path
 
-    def load_image(self):
-        img = Image.open(self.path)
-        return img
-
     def make_meme(self, img_path, text, author, width=500):
+        if width > 500:
+            raise Exception('width should be at most 500px')
 
+        img = Image.open(self.path)
+        ratio = width / float(img.size[0])
+        height = int(ratio * float(img.size[1]))
+        img = img.resize((width, height), Image.NEAREST)
 
-meme = MemeEngine('_data/photos/dog/xander_1.jpg')
-print(meme.load_image())
+        img_draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('./fonts/01211_AHDSANSB.ttf', size=20)
+        img_draw.text((10, 30), f'{text} - {author}', font=font, fill='white')
+
+        output_path = './meme.jpg'
+        img.save(output_path)
+        return output_path
